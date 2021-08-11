6958b42d8ba50328113187c76f535e874a74b719
liviabsjanchevis <88117484+liviabsjanchevis@users.noreply.github.com>
2021-08-11 17:31:22 -0300

    Create llivia_210809.py

---
+++ llivia_210809.py
@@ -0,0 +1,30 @@
+fruits=["banana","apple","pear","watermelow","peach"]
+
+print("Question3*************")
+print(fruits)
+
+print("Question4***********")
+print(fruits[0:3])
+
+print("Question5**********")
+print(fruits[2:5])
+
+print("Question6**********")
+print(fruits[1:4])
+
+print("Question7**********")
+print("naão consegui")
+
+print("Question8******")
+print("não_consegui")
+
+print("Question9*******")
+for livia in fruits[:]:
+    print(livia)
+
+print("Question10*********")
+numbers=(10,200,300)
+
+print("Question11*******")
+for livia in numbers:
+    print(livia)
